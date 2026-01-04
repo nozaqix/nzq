c691c091d03d1bb2f3b11e32f9ecb6b0a83ba00d
nozaqix <nozaqix@gmail.com>
2026-01-05 01:33:53 +0900

    SubHeader修正

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -3,26 +3,26 @@ export default function SubHeader() {
     <div className="space-y-2.5 text-xs text-[#B8B9BA] font-normal leading-normal tracking-[0.6px] font-din-next">
       <div>
         <span>Name</span>
-        <span>{' => '}</span>
-        <span>{'{nozaqi}'}</span>
+        <span>{' : '}</span>
+        <span>{'nozaqi'}</span>
       </div>
       <div>
         <span>Role</span>
-        <span>{' => '}</span>
-        <span>{'{Composer}'}</span>
+        <span>{' : '}</span>
+        <span>{'Composer'}</span>
       </div>
       <div>
-        <span>Creative</span>
-        <span>{' => '}</span>
+        <span>Works</span>
+        <span>{' : '}</span>
         <span>
-          {'{Compose | Arrangement | Lyric | Mix | Mastering}'}
+          {'Compose | Arrangement | Lyric | Mix | Mastering'}
         </span>
       </div>
       <div>
-        <span>OtherSkills</span>
-        <span>{' => '}</span>
+        <span>Skills</span>
+        <span>{' : '}</span>
         <span>
-          {'{Graphic Design | Interface Design | Concept Design | Development}'}
+          {'Compose | Graphic Design | Interface Design | Concept Design | Development | etc...'}
         </span>
       </div>
     </div>
